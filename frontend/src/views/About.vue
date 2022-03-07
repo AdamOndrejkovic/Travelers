3a704f800f61f2a0a4f8fff5cd95f1f775964a33
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-03-07 13:16:46 +0100

    About page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="imageContainer"></div>
+    <div>
+      <h1>What is Travelers?</h1>
+      <p>lorem</p>
+
+    </div>
   </div>
 </template>
+<style scoped>
+.about {
+  display: flex;
+  height: 85vh;
+  overflow: hidden;
+  padding: 10px 5px;
+  margin-top: 10px;
+  background: #F6E4AD;
+}
+
+.imageContainer {
+  background: url("../assets/images/about.jpg") no-repeat center;
+  background-size: 80% 95% ;
+  width: 50%;
+}
+
+</style>
